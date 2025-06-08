94ac58489f911e27999edf27897f5af5f74d5009
Perry <aidanpj18@gmail.com>
2025-06-07 21:03:44 -0400

    populate src

---
+++ src/verdict/analyzer.lua
@@ -0,0 +1,3 @@
+local analyzer = {}
+
+return analyzer
---
+++ src/verdict/init.lua
@@ -0,0 +1,9 @@
+local analyzer = require("verdict.analyzer")
+
+local verdict = {}
+
+verdict.analyzer = analyzer
+
+print("verdict module loaded")
+
+return verdict
