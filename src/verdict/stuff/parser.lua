59fcf5d1755e4c6b6deab378d950c42bfaf1339d
Perry <aidanpj18@gmail.com>
2025-06-09 17:48:25 -0400

    start implementing parser and re-doc

--- src/verdict/stuff/parser.lua
+++ src/verdict/stuff/parser.lua
@@ -3,4 +3,30 @@
 local Parser = {}
 Parser.__index = Parser
 
+--- Creates a new AST node
+---@param type string The node type
+---@param data table Additional node data
+---@param line number Line number
+---@param column number Column number
+---@return table A new AST node
+local function create_node(type, data, line, column)
+    return {
+        type = type,
+        data = data or {},
+        line = line,
+        column = column
+    }
+end
+
+--- Creates a new Parser instance
+---@param tokens table Array of tokens from lexer
+---@return table a new Parser instance
+function Parser.new(tokens)
+    return setmetatable({
+        tokens = tokens,
+        position = 1,
+        current_token = tokens[1]
+    }, Parser)
+end
+
 return Parser
